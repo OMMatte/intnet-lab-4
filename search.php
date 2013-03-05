91df30f97adf7f2af70e3c959a318b37b227849c
Lucas Wiener <lucas.wiener@trustly.com>
2013-03-05 17:54:28 +0100

    debug

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>PHP/PostgreSQL-test</title>
 	<script type="application/javascript">
